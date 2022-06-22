c96b13347cdeac5e5cafe43b13e7efb77ba32ec4
jsupino <98129294+jsupino@users.noreply.github.com>
2022-06-21 21:56:18 -0400

    Update SW_Python.py

--- SW_Python.py
+++ SW_Python.py
@@ -9,7 +9,7 @@ from collections import Counter
 
 
 #download necessary files (downloaded most popular)
-# nltk.download()
+nltk.download()
 
 
 #create a list: separated by each line: line number, character name, and dialogue
@@ -27,7 +27,7 @@ with open("C:/Users/supin/Desktop/starwars/SW_EpisodeV.txt", "r") as text:
             "dialogue": dialogue
         }
         sw_dic.append(new_obj)
-# print(sw_dic)
+print(sw_dic)
 
 #create a numpy.ndarray of all names
 arr = np.array(sw_dic)
@@ -52,11 +52,11 @@ print("The most active character in this episode is: ",most_frequent(name_column
 
 #only the dictionary keys (character names)
 char_name_dic = name_freq.keys()
-# print(char_name_dic)
+print(char_name_dic)
 
 #only the dictionary values (how many lines each character has)
 freq_dic = name_freq.values()
-# print(freq_dic)
+print(freq_dic)
 
 #create a bar chart comparing characters and the number of lines they have
 Character = char_name_dic 
@@ -71,19 +71,17 @@ plt.yticks(fontsize = 8)
 plt.show()
 
 
-
-
 #create a numpy.ndarray of the dialogue
 dialogue_arr = np.array(sw_dic)
 #turn it into a list of the dialogue
 char_lines = [row['dialogue'] for row in dialogue_arr]
-# print(char_lines)
+print(char_lines)
 
 #remove punctuation
 import string
 excluded = set(string.punctuation)
 char_lines = ' '.join(ch for ch in char_lines if ch not in excluded)
-# print(char_lines)
+print(char_lines)
 
 #remove the rest of the punctuation
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -92,12 +90,12 @@ for char_punc in char_lines:
     if char_punc not in punc:
         no_punc_lines = no_punc_lines + char_punc
 no_punc_lines = no_punc_lines
-# print(no_punc_lines)
+print(no_punc_lines)
 
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-# nltk.download('stopwords')
+nltk.download('stopwords')
 
 #remove stop words from the dialogue
 stopWords = set(stopwords.words('english'))
@@ -106,24 +104,24 @@ filtered_dialogue = []
 for w in words:
     if w not in stopWords:
         filtered_dialogue.append(w)
-# print(filtered_dialogue)
+print(filtered_dialogue)
 
 #create a list of the 20 most common words used throughout the episode
 Count_no = Counter(filtered_dialogue)
 common_words = Count_no.most_common(20)
-# print(common_words)
+print(common_words)
 
 #Turn the list into a dictionary of the 20 most common words
 common_words_dic = dict(common_words)
-# print(common_words_dic)
+print(common_words_dic)
 
 #only the dictionary keys (words)
 most_freq_words_used = common_words_dic.keys()
-# print(most_freq_words_used)
+print(most_freq_words_used)
 
 #only the dictionary values (how often the word occurs)
 most_freq_words_used_values = common_words_dic.values()
-# print(most_freq_words_used_values)
+print(most_freq_words_used_values)
 
 #create a bar chart comparing 20 most common words and how often they occur
 Word = most_freq_words_used
